fca75abfd03a7780fd8e9f258a3c94e5d1b94675
2flow <flo@wimmer.kiwi>
2021-09-29 11:38:55 +0200

    Add * operator to LED

--- include/LED.h
+++ include/LED.h
@@ -13,6 +13,17 @@ public:
     float red = 0;
     float blue = 0;
     float green = 0;
+
+    LED operator*(float value) const{
+        return LED{red * value, green * value, blue * value};
+    }
+
+    LED operator*(double _value) const{
+        auto value = static_cast<float>(_value);
+        return LED{red * value, green * value, blue * value};
+    }
+
+
 };
 
 #endif //UNTITLED_LED_H
